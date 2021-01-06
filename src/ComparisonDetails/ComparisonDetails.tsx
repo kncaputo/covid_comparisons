d99312c6cbd8eaee839aceb78d98f167269ddce6
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-05 20:01:34 -0700

    Create scss files for all functional components

--- src/ComparisonDetails/ComparisonDetails.tsx
+++ src/ComparisonDetails/ComparisonDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './ComparisonDetails.scss';
 
 const ComparisonDetails = () => {
 
--- src/USAState/USAState.tsx
+++ src/USAState/USAState.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './USAState.scss';
 
 const USAState = () => {
 }
