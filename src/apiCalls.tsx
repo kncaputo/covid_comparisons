0c9dec64394c261c96e6163016816bb43a559883
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-06 15:26:40 -0700

    Add error handling for response

--- src/apiCalls.tsx
+++ src/apiCalls.tsx
@@ -1,6 +1,12 @@
 export const fetchCurrentStateData = () => {
   return (
     fetch('https://api.covidtracking.com/v1/states/co/current.json')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw Error('Failed to fetch data')
+        }
+      return response.json()
+      }
+    )
   )
 }
