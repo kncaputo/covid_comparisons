0577741545e6628bf44490f977e75c1b10a152fb
Kara Caputo <kncaputo@gmail.com>
2021-01-07 17:25:45 -0500

    Add inital test for app

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import USAState from '../USAState/USAState';
+import ComparisonContainer from '../ComparisonContainer/ComparisonContainer';
+import Stat from '../Stat/Stat';
+import { fetchAllCurrentUSAData, fetchCurrentStateData } from '../apiCalls'
+import { singleState, usaData } from '../sampleData';
+import { GiCoffin } from 'react-icons/gi';
+import { RiVirusFill, RiHospitalFill } from 'react-icons/ri';
+import '@testing-library/jest-dom';
+import '@types/jest';
+jest.mock('../apiCalls');
+ 
+describe('App', () => {
+  beforeEach(() => {
+    const mockedUSAFetch = fetchAllCurrentUSAData as jest.MockedFunction<() => any>
+    const mockedSingleFetch = fetchCurrentStateData as jest.MockedFunction<() => any>
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    mockedUSAFetch.mockResolvedValueOnce(usaData[0])
+    mockedSingleFetch.mockResolvedValueOnce(singleState)
+
+    render(
+      <App />
+    );
+  });
+
+  it('should render correctly', () => {
+    const tagline = screen.getByText('Covid Comparisons');
+    
+    expect(tagline).toBeInTheDocument();
+  });
 });
+
+
+    // const mockedFetch = fetchAllCurrentUSAData as jest.Mocked <typeof any>
