03ee35b132b9a93ef943072c42c83a9c70f28dae
Kara Caputo <kncaputo@gmail.com>
2021-01-07 18:01:19 -0500

    Complete render test for app

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
-import USAState from '../USAState/USAState';
-import ComparisonContainer from '../ComparisonContainer/ComparisonContainer';
-import Stat from '../Stat/Stat';
 import { fetchAllCurrentUSAData, fetchCurrentStateData } from '../apiCalls'
 import { singleState, usaData } from '../sampleData';
-import { GiCoffin } from 'react-icons/gi';
-import { RiVirusFill, RiHospitalFill } from 'react-icons/ri';
 import '@testing-library/jest-dom';
 jest.mock('../apiCalls');
  
 describe('App', () => {
   beforeEach(() => {
-    const mockedUSAFetch = fetchAllCurrentUSAData as jest.MockedFunction<() => any>
-    const mockedSingleFetch = fetchCurrentStateData as jest.MockedFunction<() => any>
+    const mockedUSAFetch = fetchAllCurrentUSAData as jest.MockedFunction<typeof fetchAllCurrentUSAData>
+    const mockedSingleFetch = fetchCurrentStateData as jest.MockedFunction<typeof fetchCurrentStateData>
 
     mockedUSAFetch.mockResolvedValueOnce(usaData[0])
     mockedSingleFetch.mockResolvedValueOnce(singleState)
@@ -26,10 +20,23 @@ describe('App', () => {
 
   it('should render correctly', () => {
     const tagline = screen.getByText('Covid Comparisons');
+    const overviewTitle = screen.getByText('USA Overview:');
+    const stateName = screen.getByText('Colorado');
+    const cases = screen.getByText('Cases');
+    const deaths = screen.getByText('Deaths');
+    const currentHospitalizations = screen.getByText('Current Hospitalizations');
+    const howDoesItCompare = screen.getByText('How Does It Compare?')
+    const dropdown = screen.getByTestId('dropdown')
+    const casesIcon = screen.getByTestId('cases')
     
     expect(tagline).toBeInTheDocument();
+    expect(overviewTitle).toBeInTheDocument();
+    expect(stateName).toBeInTheDocument();
+    expect(cases).toBeInTheDocument();
+    expect(deaths).toBeInTheDocument();
+    expect(currentHospitalizations).toBeInTheDocument();
+    expect(howDoesItCompare).toBeInTheDocument();
+    expect(dropdown).toBeInTheDocument();
+    expect(casesIcon).toBeInTheDocument();
   });
 });
-
-
-    // const mockedFetch = fetchAllCurrentUSAData as jest.Mocked <typeof any>
