d83d804e9ee990f097331dafa2874a0a84abf21a
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-06 17:22:30 -0700

    Remove conditional render, dynamically add icons

--- src/Stat/Stat.tsx
+++ src/Stat/Stat.tsx
@@ -1,19 +1,9 @@
-import { GiCoffin } from 'react-icons/gi';
-import { RiHospitalFill, RiVirusFill } from 'react-icons/ri';
 import './Stat.scss';
 
 const Stat = (prop: {icon: any, number: number, title: string, details: string}) => {
   return(
     <section>
-      { prop.icon === GiCoffin && 
-        <GiCoffin/>
-      }
-      { prop.icon === GiCoffin && 
-        <RiVirusFill/>
-      }
-      { prop.icon === GiCoffin && 
-        <RiHospitalFill/>
-      }
+      { <prop.icon/> }
       <p>{ prop.number }</p>
       <p>{ prop.title }</p>
       <p>{ prop.details }</p>
