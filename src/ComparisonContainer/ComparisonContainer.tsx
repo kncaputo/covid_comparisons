fbc601bfc1d4364c6e327a694bb7ccf464825f10
Kara Caputo <kncaputo@gmail.com>
2021-01-10 15:49:34 -0500

    Restructure return of ComparisonContainer

--- src/ComparisonContainer/ComparisonContainer.tsx
+++ src/ComparisonContainer/ComparisonContainer.tsx
@@ -27,31 +27,41 @@ const ComparisonContainer = (prop: { className: string, handleComparisonClick(dr
     <section className='comparison-container'>
       <h3 className='comparison-title'>How Does It Compare?</h3>
       <section>
-        <section className='app-description'>
-          <p>The number of deaths from the coronavirus is astounding but 
-            is often hard to picture the magnitude of.</p>
-          <p className='paragraph2'>Select an event from 
-            the dropdown menu below to see how the deaths in this state compare 
-            to other castastrophic events in history.</p>
+
+        <article id='app-description-container'>
+          <section id='app-description'>
+            <p>
+              The number of deaths from the coronavirus is astounding but is often hard to picture the magnitude of.
+            </p>
+            <p className='paragraph2'>
+              Select an event from the dropdown menu below to see how the deaths in this state compare to other castastrophic events in history.
+            </p>
+          </section>
+        </article>
+
+        <section id='dropdown-container'>
+          <select 
+            id='dropdown' 
+            data-testid='dropdown'
+            onChange={handleChange}>
+            <option 
+              id='dropdown-default'
+              key='dropdown-default'
+              value='select-a-comparison'
+              disabled
+              selected>
+              Select a Comparison Category
+            </option>  
+            {createDropdownValues()}
+          </select>
+          <section>
+            <Link to={`/${dropdownValue}`}>
+              <button className='comparison-button' onClick={() => prop.handleComparisonClick(dropdownValue)}>View Comparison</button>
+            </Link>
+          </section>
         </section>
-        <select 
-          id='dropdown' 
-          data-testid='dropdown'
-          onChange={handleChange}>
-          <option 
-            id='dropdown-default'
-            key='dropdown-default'
-            value='select-a-comparison'
-            disabled
-            selected>
-            Select a Comparison Category
-          </option>  
-          {createDropdownValues()}
-        </select>
+
       </section>
-      <Link to={`/${dropdownValue}`}>
-        <button className='comparison-button' onClick={() => prop.handleComparisonClick(dropdownValue)}>View Comparison</button>
-      </Link>
     </section>
   )
 }
