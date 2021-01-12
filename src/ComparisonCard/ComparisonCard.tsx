6dfbfd1b496d4663c3881a98965c56126520c865
Kara Caputo <kncaputo@gmail.com>
2021-01-12 09:51:25 -0500

    Add grid to ComparisonDetails and flex wrap styles to body of ComparisonCard

--- src/ComparisonCard/ComparisonCard.tsx
+++ src/ComparisonCard/ComparisonCard.tsx
@@ -4,9 +4,9 @@ import { Comparison } from '../comparisonData';
 const ComparisonCard = (prop: {title: string, body: JSX.Element[], styleId: string, image?: string}) => {
   const generateComparisonCards = () => {
     return(
-      <section className={`cards ${`{styleId}`}`}>
+      <section className={`${prop.styleId}-cards`}>
         <h1>{prop.title}</h1>
-        <p>{prop.body}</p>
+        <section className={`${prop.styleId}}-body`}>{prop.body}</section>
       </section>
     )
   }
